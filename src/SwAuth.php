632d03b2fc3dfc0980a19a17d09a6d742d9c64f0
Samuel <samuelcchaves2002@icloud.com>
2026-09-13 15:30:30 +0100

    implementação dotenv

--- src/SwAuth.php
+++ src/SwAuth.php
@@ -1,8 +1,26 @@
 <?php
 namespace SwAuth;
 
+use Dotenv\Dotenv;
+
+
 final class SwAuth
 {
+    private static bool $booted = false;
+
+    public static function boot(string $rootPath): void
+    {
+        if(self::$booted) {
+            return;
+        }
+
+        $dotenv = Dotenv::createImmutable($rootPath);
+        $dotenv->load();
+
+        self::$booted = true;
+
+    }
+
     public static function version(): string
     {
         return '1.0.0';
