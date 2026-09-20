4a5ee97e2c5f1b20570268b0a1db7168181c9b2a
Samuel <samuelcchaves2002@icloud.com>
2026-09-20 21:20:52 +0100

    unfolded method save into insert and update like the other interfaces

--- src/Domain/Repositories/MfaMethodRepositoryInterface.php
+++ src/Domain/Repositories/MfaMethodRepositoryInterface.php
@@ -4,16 +4,24 @@ namespace SwAuth\Domain\Repositories;
 
 use SwAuth\Domain\Entities\MfaMethod;
 
-use SwAuth\Domain\ValueObjects\TokenHash;
+use SwAuth\Domain\ValueObjects\BackupCode;
 
 interface MfaMethodRepositoryInterface 
 {
+
+    public function findById(int $userId): ?MfaMethod;
+
     public function findActiveByUserId(int $userId): ?MfaMethod;
-    public function save(MfaMethod $mfaMethod): void;
+
+    public function insert(MfaMethod $mfaMethod): ?MfaMethod;
+
+    public function update(MfaMethod $mfaMethod): void;
+
     public function deactivate(int $mfaMethodId): void;
-
-    public function saveBackUpCodes(int $userId, array $codeHashes): void;
-
-    public function consumeBackupCode(int $userId, TokenHash $codeHash): bool;
+    /**
+     *  @param BackupCode[] $codeHashes
+    */
+    public function saveBackupCode(int $userId, array $codeHashes): void;
+    public function consumeBackupCode(int $userId, BackupCode $codeHash): bool;
 
 }
