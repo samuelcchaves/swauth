d8976313d6b0070e5069982067930d54b4a19276
Samuel <samuelcchaves2002@icloud.com>
2026-09-20 21:16:07 +0100

    bug fix in isvalid with inverted logic

--- src/Domain/Entities/Session.php
+++ src/Domain/Entities/Session.php
@@ -78,7 +78,7 @@ class Session {
 
     public function isValid(): bool
     {
-        return $this->isExpired() && !$this->isRevoked();
+        return !$this->isExpired() && $this->isRevoked();
     }
 
     public function revoke(): void 
@@ -90,11 +90,5 @@ class Session {
     {
         return $this->tokenHash->matches($rawToken);
     }
-    
-
-
-
-
-
-   
+      
 }
