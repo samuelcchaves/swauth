19b9c550133ca15bb5e649c60cf9a6e11af1c0b6
Samuel <samuelcchaves2002@icloud.com>
2026-09-22 20:21:36 +0100

    replaced save by insert and update

--- src/Domain/Repositories/PasswordResetRepositoryInterface.php
+++ src/Domain/Repositories/PasswordResetRepositoryInterface.php
@@ -3,14 +3,17 @@
 
 namespace SwAuth\Domain\Repositories;
 
+use PhpParser\Token;
 use SwAuth\Domain\Entities\PasswordResetToken;
 use SwAuth\Domain\ValueObjects\TokenHash;
 
 interface PasswordResetRepositoryInterface
 {
     public function findPendingByTokenHash(TokenHash $tokenHash): ?PasswordResetToken;
-    public function save(PasswordResetToken $token): void;
     public function supersedePendingForUser(int $userId): void;
     
+    public function insert(TokenHash $tokenHash): TokenHash;
+    public function update(TokenHash $tokenHash): void;
+
 }
 
